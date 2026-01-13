f22146e3e35a7b59bf3555a30490130143878708
LinaOuchaouAmroussi <lina.ouchaou@edu.esiee.fr>
2026-01-13 16:05:44 +0100

    modification workflow

--- td5/scripts/tofu/live/lambda-sample/main.tf
+++ td5/scripts/tofu/live/lambda-sample/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-# 1. Rôle IAM
+# 1. Rôle IAM pour la Lambda
 resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda_lina_final" # Un nom tout neuf
+  name = "iam_for_lambda_lina_final"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -18,24 +18,25 @@ resource "aws_iam_role" "iam_for_lambda" {
   })
 }
 
-# 2. Code source
+# 2. Archive du code source
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/src"
   output_path = "${path.module}/lambda.zip"
 }
 
-# 3. Lambda
+# 3. La fonction Lambda
 resource "aws_lambda_function" "hello_world" {
   filename      = data.archive_file.lambda_zip.output_path
   function_name = "lambda-sample-lina-final"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.handler"
   runtime       = "nodejs20.x"
+
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
 
-# 4. API Gateway
+# 4. API Gateway (L'entrée HTTP pour accéder à ta Lambda)
 resource "aws_apigatewayv2_api" "lambda_api" {
   name          = "lambda-api-lina-final"
   protocol_type = "HTTP"
@@ -59,7 +60,7 @@ resource "aws_apigatewayv2_route" "default_route" {
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
-# 5. Permission
+# 5. Permission pour que l'API puisse appeler la Lambda
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -68,6 +69,8 @@ resource "aws_lambda_permission" "api_gw" {
   source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
 }
 
+# 6. OUTPUTS : C'est ici que l'URL s'affichera
 output "api_url" {
-  value = aws_apigatewayv2_stage.default.invoke_url
+  description = "URL à copier dans ton navigateur pour le rapport"
+  value       = aws_apigatewayv2_stage.default.invoke_url
 }
